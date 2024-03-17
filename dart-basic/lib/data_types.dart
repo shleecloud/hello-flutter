2a0e95760ff0a29860cf25d94890b94046977e25
shleecloud <shleecloud@gmail.com>
2024-03-17 22:25:54 +0900

    sets

--- dart-basic/lib/data_types.dart
+++ dart-basic/lib/data_types.dart
@@ -7,6 +7,7 @@ void dataTypes() {
   int age = 30;
   double height = 1.75;
 
+  // * collection if
   var giveMeFive = true;
   var numbers = [
     1,
@@ -20,6 +21,7 @@ void dataTypes() {
 
   print("Hello my age is $age! and next year I will be ${age + 1} years old.");
 
+  // * collection for
   var oldFriends = ['Alice', 'Bob', 'Charlie'];
   var newFriends = [
     'David',
@@ -28,4 +30,47 @@ void dataTypes() {
     for (var friend in oldFriends) '💖 $friend'
   ];
   print(newFriends); // [💖 Alice, 💖 Bob, 💖 Charlie]
+
+  // * Maps
+  // Object 타입은 모든 타입을 받을 수 있다. any와 같은 개념이다.
+  var person = {
+    'name': 'John Doe',
+    'age': 30,
+    'isMarried': false,
+    'weight': 70,
+    'height': 1.75,
+  };
+  print(person['name']); // John Doe
+
+  Map<List<int>, String> map = {
+    [1, 2, 3]: 'Hello',
+    [4, 5, 6]: 'World',
+  };
+
+  // * Maps with type
+  // dynamic 타입은 모든 타입을 받을 수 있다. any와 같은 개념이다.
+  List<Map<String, dynamic>> people = [
+    {
+      'name': 'John Doe',
+      'age': 30,
+      'isMarried': false,
+      'weight': 70,
+      'height': 1.75,
+    },
+    {
+      'name': 'Jane Dan',
+      'age': 25,
+      'isMarried': true,
+      'weight': 60,
+      'height': 1.65,
+    },
+  ];
+
+  // * Sets
+  Set<int> numbersSet = {1, 2, 3, 4, 5};
+  numbersSet.add(6);
+  numbersSet.add(6);
+  numbersSet.add(6);
+  numbersSet.add(6);
+  print(numbersSet); // {1, 2, 3, 4, 5, 6}
 }

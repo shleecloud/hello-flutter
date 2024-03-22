b704ced26d6d9ffe51253ecabf9da86374f4394a
shleecloud <shleecloud@gmail.com>
2024-03-23 01:36:10 +0900

    setup 240323

--- dart-basic/bin/main.dart
+++ dart-basic/bin/main.dart
@@ -1,9 +1,15 @@
 // import 'package:dart_basic/dart_basic.dart' as dart_basic;
-import 'package:dart_basic/variables.dart' as variables;
-import 'package:dart_basic/data_types.dart' as data_types;
-import 'package:dart_basic/functions.dart' as functions;
-import 'package:dart_basic/classes.dart' as classes;
-import 'package:dart_basic/inheritance.dart' as inheritance;
+// import 'package:dart_basic/variables.dart' as variables;
+// import 'package:dart_basic/data_types.dart' as data_types;
+// import 'package:dart_basic/functions.dart' as functions;
+// import 'package:dart_basic/classes.dart' as classes;
+// import 'package:dart_basic/inheritance.dart' as inheritance;
+
+import '../lib/variables.dart' as variables;
+import '../lib/data_types.dart' as data_types;
+import '../lib/functions.dart' as functions;
+import '../lib/classes.dart' as classes;
+import '../lib/inheritance.dart' as inheritance;
 
 void main() {
   variables.variables();
